52235fbf7fc2f124b427b2b26f0a1522d14ed6ac
Mohammad Ali <gobalee10@gmail.com>
2025-02-07 04:02:49 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart'; 
 import 'screens/home.dart';
 
 void main() {
@@ -8,7 +9,27 @@ void main() {
     DeviceOrientation.landscapeLeft,
     DeviceOrientation.landscapeRight,
   ]).then((_) {
-    runApp(const RestaurantHomePage());
+    runApp(const karobarApp());
   });
 }
 
+class karobarApp extends StatelessWidget {
+  const karobarApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(1300, 600),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Responsive Restaurant App',
+          theme: ThemeData(),
+          home: const RestaurantHomePage(),
+        );
+      },
+    );
+  }
+}
